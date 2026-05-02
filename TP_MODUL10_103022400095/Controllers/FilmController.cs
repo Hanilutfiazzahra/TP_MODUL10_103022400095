85baeac0c507310354acb41cc1ce01207d11f792
Hanilutfiazzahra <hanilutfiazzahraa@gmail.com>
2026-05-02 23:17:06 +0700

    Menambahkan API Film

---
+++ TP_MODUL10_103022400095/Controllers/FilmController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TP_MODUL10_103022400095.Models;
+using TP_MODUL10_103022400095.Models;
+
+namespace TP_MODUL10_103022400095.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilmController : ControllerBase
+    {
+        private static List<Film> filmList = new List<Film>
+        {
+            new Film
+            {
+                Judul = "Inception",
+                Sutradara = "Christopher Nolan",
+                Tahun = "2010",
+                Genre = "Sci-Fi",
+                Rating = "9.0"
+            },
+            new Film
+            {
+                Judul = "Interstellar",
+                Sutradara = "Christopher Nolan",
+                Tahun = "2014",
+                Genre = "Sci-Fi",
+                Rating = "8.7"
+            },
+            new Film
+            {
+                Judul = "Parasite",
+                Sutradara = "Bong Joon-ho",
+                Tahun = "2019",
+                Genre = "Thriller",
+                Rating = "8.6"
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Film>> GetAll()
+        {
+            return filmList;
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Film> GetByIndex(int index)
+        {
+            if (index < 0 || index >= filmList.Count)
+            {
+                return NotFound();
+            }
+
+            return filmList[index];
+        }
+
+        [HttpPost]
+        public ActionResult AddFilm([FromBody] Film film)
+        {
+            filmList.Add(film);
+            return Ok(film);
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult DeleteFilm(int index)
+        {
+            if (index < 0 || index >= filmList.Count)
+            {
+                return NotFound();
+            }
+
+            filmList.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
--- TP_MODUL10_103022400095/Program.cs
+++ TP_MODUL10_103022400095/Program.cs
@@ -1,19 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
-// test commit
 
-// Add services to the container.
-
+// Services
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// Middleware
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
